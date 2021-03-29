375b2addde705f8b16193b5a7aa18d555ad7a8db
Thomas GOETZMANN <thomas.goetzmann@gmail.com>
2021-03-29 20:22:50 +0200

    First running version

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using McMaster.Extensions.CommandLineUtils;
+using Dir = System.IO.Directory;
+
+namespace CSharpSourceCodeToSingleFile
+{
+    [Command(ExtendedHelpText = @"
+Searches through all .cs files in a given folder (recursive) and aggregates them in a single .cs output file.")]
+    class Program
+    {
+        static void Main(string[] args)
+            => CommandLineApplication.Execute<Program>(args);
+
+        [DirectoryExists]
+        [Option(Description = "Folder path were all .cs files are located (search is recursive). Default is Current Directory.", ShortName = "d")]
+        public string Directory { get; } = ".";
+
+        [Required]
+        [FileNotExists]
+        [FileExtensions(Extensions = ".cs")]
+        [Option(Description = "Single file name the code will be merged into", ShortName = "o")]
+        public string Output { get; }
+
+        [Option(CommandOptionType.NoValue)]
+        public bool Verbose { get; }
+
+        private void OnExecute()
+        {
+            var directory = Directory == "." ? Environment.CurrentDirectory : Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, Directory));
+            var outputPath = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, Output));
+
+            var objFolder = $"{Path.DirectorySeparatorChar}obj{Path.DirectorySeparatorChar}";
+            var binFolder = $"{Path.DirectorySeparatorChar}bin{Path.DirectorySeparatorChar}";
+
+            var files = Dir
+                .EnumerateFiles(directory, "*.cs", SearchOption.AllDirectories)
+                .Where(path => !path.Contains(binFolder)
+                            && !path.Contains(objFolder));
+
+            if(Verbose)
+            {
+                Console.WriteLine("File found:");
+                foreach(var f in files)
+                    Console.WriteLine(f);
+            }
+
+            var lines = files
+                .SelectMany(file => File.ReadAllLines(file));
+
+            var usings = lines
+                .Where(line => line.StartsWith("using"))
+                .Distinct();
+
+            var classes = lines.Where(line => !usings.Contains(line));
+
+            var outputDirectory = Path.GetDirectoryName(outputPath);
+            System.IO.Directory.CreateDirectory(outputDirectory);
+
+            using (var writer = File.CreateText(outputPath))
+            {
+                foreach (var line in usings)
+                    writer.WriteLine(line);
+
+                writer.WriteLine();
+
+                foreach (var line in classes)
+                    writer.WriteLine(line);
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"SUCCESS - Single source code file generated. {outputPath}");
+            Console.ResetColor();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # CSharpSourcesToSingleFile
-Takes all .cs files in a directory and combines them in a single .cs output file (console application)
+
+Takes all .cs files in a directory and combines them in a single .cs output file (console application).
+
+The search is done in the **given directory** and is **recursive** but **excludes** the ```/bin/``` and ```/obj``` folders.
+
+## Usage
+
+You can run the executable with different args and use the --help argument for more details.
+
+```bash
+.\CSharpSourcesToSingleFile.exe --help
+```
+
+Hint: Add the folder containing the .exe to your Path for an easier usage.
+
+## Screenshots
+
+![Usage example](img/usage-example.png)
+
+## Build
+
+You can download and extract the .zip archive containing the compiled files, or you can build it yourself with the following commands.
+
+**WARNING:** The project uses **.Net 5.0**
+
+```bash
+# Restores the NuGet package dependencies
+dotnet restore
+
+# Build the .exe
+dotnet build
+```
